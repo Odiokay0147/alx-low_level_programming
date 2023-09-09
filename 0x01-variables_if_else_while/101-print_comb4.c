da8019b0b2d6bceaf3b177594271667a67a46024
Odiokay0147 <odionkassim0011@gmail.com>
2023-09-09 08:13:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,25 +8,25 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n < 58; n++)
+	for (n = 1; n < 4; n++)
 	{
-		for (m = 49; m < 58; n++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+	for (m = 2; m < 4; n++)
+	{
+	for (l = 3; l < 4; l++)
+	{
+	if (l > m && m > n)
+	{
+	putchar(n);
+	putchar(m);
+	putchar(l);
+	if (n != 8 || m != 9)
+	{
+	putchar(',');
+        putchar(' ');
+	}
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
