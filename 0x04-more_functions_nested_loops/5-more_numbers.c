951586605fccfa6d0c9e99a3656ef417f2402ba0
Odiokay0147 <odionkassim0011@gmail.com>
2023-09-15 05:19:54 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,20 @@
  * Return: 10 times of the numbers from 0 up to 14
  */
 
-void more_number(void)
+void more_numbers(void)
 
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
-	if (y > 9)
+	if (y >= 10)
 	{
-	_putchar((y / 10) + '0');
+	_putchar('1');
 	}
-	_putchar((y % 10) + '0');
+	_putchar(y % 10 + '0');
 	}
 	_putchar('\n');
 	}
