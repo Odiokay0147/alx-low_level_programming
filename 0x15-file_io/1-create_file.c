8c424abfa3617c2036e7012407d53714ee25c3f4
odionkassim <odionkassim0011@gmail.com>
2023-10-30 18:43:32 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (letters = 0; text_content[letters]; letters++)
+
 	rwr = write(fd, text_content, letters);
 
 	if (rwr == -1)
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
