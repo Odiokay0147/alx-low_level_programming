836682f9ca23bacb6947a72c4290b65858e69be1
Odiokay0147 <odionkassim0011@gmail.com>
2024-01-10 16:13:05 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-/**_puts_recursion - prints a string, followed by a new line
+/**
+ * _puts_recursion - prints a string, followed by a new line
  * @s: strint to print
  *
  * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
